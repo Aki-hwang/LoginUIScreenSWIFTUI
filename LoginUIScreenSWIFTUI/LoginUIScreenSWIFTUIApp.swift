79d7495e5dab8ecdbdbd15ccb0ca75fbc840bdc2
chuljin Hwang <chljin.hwang@gmail.com>
2022-02-24 15:09:38 +0900

    Initial Commit

---
+++ LoginUIScreenSWIFTUI/LoginUIScreenSWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUIScreenSWIFTUIApp.swift
+//  LoginUIScreenSWIFTUI
+//
+//  Created by Chuljin Hwang on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginUIScreenSWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
